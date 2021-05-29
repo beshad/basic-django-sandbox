245d7541716bb79ca40b8a59eb5f39c5512a530b
Behshad Ghorbani <behshad.ghorbani@truescape.com>
2021-05-29 15:51:31 +1200

    admin page customized

--- pages/admin.py
+++ pages/admin.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
 from .models import Content
 
+from django.contrib.auth.models import Group
+admin.site.unregister(Group)
+
+admin.site.site_header = 'My Sandbox Django Admin Page'
+admin.site.site_title  =  "Custom Site Title"
+admin.site.index_title  =  "Custom index Admin"
+
 class ContentAdmin(admin.ModelAdmin):
-    fields = ['username','password']
+    fields = ['username', 'password']
+
+    list_display = ("username", "password")
 
 admin.site.register(Content, ContentAdmin)
--- pages/models.py
+++ pages/models.py
@@ -1,5 +1,13 @@
 from django.db import models
 
+
 class Content(models.Model):
     username = models.CharField(max_length=30)
     password = models.CharField(max_length=30)
+
+    def __str__(self):
+        return f"{self.username}, {self.password}"
+
+    class Meta:
+        verbose_name_plural = "Content"
+        verbose_name_plural  =  "Contents"    
